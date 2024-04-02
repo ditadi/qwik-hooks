e0dd12f44b0a9a8d7d7c9dfada8b0e245b6eb0b3
maiieul <maieul.chevalier@gmail.com>
2024-04-02 20:25:24 +0200

    fix whitespace differences

--- src/use-window-size.ts
+++ src/use-window-size.ts
@@ -2,35 +2,35 @@ import { $, useOnWindow, useSignal, useTask$ } from "@builder.io/qwik";
 import { isServer } from "@builder.io/qwik/build";
 
 type WindowSize = {
-  width: number;
-  height: number;
+    width: number;
+    height: number;
 };
 
 const useWindowSize = () => {
-  const size = useSignal<WindowSize>();
+    const size = useSignal<WindowSize>();
 
-  const handleResize = $(() => {
-    size.value = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  });
+    const handleResize = $(() => {
+        size.value = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
+    });
 
-  /**
-   * useTask$ triggers when the hooks is called after client-side routing.
-   * useOnWindow("load") triggers when the page is loaded server-side.
-   * useOnWindow("resize") triggers when the page is resized.
-   */
+    /**
+     * useTask$ triggers when the hooks is called after client-side routing.
+     * useOnWindow("load") triggers when the page is loaded server-side.
+     * useOnWindow("resize") triggers when the page is resized.
+     */
 
-  useTask$(() => {
-    if (isServer) return;
-    handleResize();
-  });
+    useTask$(() => {
+        if (isServer) return;
+        handleResize();
+    });
 
-  useOnWindow("load", handleResize);
-  useOnWindow("resize", handleResize);
+    useOnWindow("load", handleResize);
+    useOnWindow("resize", handleResize);
 
-  return size;
+    return size;
 };
 
 export { useWindowSize };
