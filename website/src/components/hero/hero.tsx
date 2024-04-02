b91281f688e432d8132ece1e3d747fc33882e8c3
Victor Ditadi <victordperd@gmail.com>
2024-04-02 17:01:43 +0100

    fix: 🐛 fix hero message

--- website/src/components/hero/hero.tsx
+++ website/src/components/hero/hero.tsx
@@ -8,8 +8,12 @@ export default component$(() => {
             <QwikHooksLogo width={"clamp(20rem,50vw,30rem)"} />
             <div class="flex flex-col max-w-lg">
                 <h1 class="text-white transform-none font-sans text-[clamp(1.2rem,2.5vw,1.5rem)]">
-                    A collection of modern, server-safe React hooks – from the ui.dev team
+                    A collection of modern qwik hooks.
+                    <br />
                 </h1>
+                <h2 class="text-white transform-none font-sans text-[clamp(1rem,2.2vw,1.2rem)]">
+                    from @victorditadi
+                </h2>
             </div>
             <div class="w-full max-w-4xl">
                 <Install bgVariant="foreground" path="npm i -D @ditadi/qwik-hooks" />
