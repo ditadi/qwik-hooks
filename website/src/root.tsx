cf5edf57ee601d188ce674c4dd11405a4b25332f
Victor Ditadi <victordperd@gmail.com>
2024-04-03 12:29:42 +0100

    ci: 🎡 add vercel analytics

--- website/src/root.tsx
+++ website/src/root.tsx
@@ -1,9 +1,16 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { RouterHead } from "./components/router-head/router-head";
+
 import "./global.css";
 
 export default component$(() => {
+    useVisibleTask$(() => {
+        inject();
+        injectSpeedInsights();
+    });
     return (
         <QwikCityProvider>
             <head>
